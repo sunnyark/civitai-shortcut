c116d41f7ccbdd980193d38ae9c507ce0811851c
陳鈞 <jim60105@gmail.com>
2024-02-12 02:34:26 +0800

    refactor: Auto format document

--- scripts/civitai_manager_libs/setting.py
+++ scripts/civitai_manager_libs/setting.py
@@ -17,12 +17,12 @@ Extensions_Name = "Civitai Shortcut"
 Extensions_Version = "v1.6.7"
 
 PLACEHOLDER = "[No Select]"
-NORESULT = "[No Result]"  
+NORESULT = "[No Result]"
 NEWRECIPE = "[New Prompt Recipe]"
 NEWCLASSIFICATION = "[New Classification]"
 
-CREATE_MODEL_FOLDER = "Create a model folder to download the model"    
-# CREATE_MODEL_FOLDER = "Create a model folder with the model name"    
+CREATE_MODEL_FOLDER = "Create a model folder to download the model"
+# CREATE_MODEL_FOLDER = "Create a model folder with the model name"
 
 model_exts = (".bin", ".pt", ".safetensors", ".ckpt")
 
@@ -56,17 +56,17 @@ model_basemodels = {
 model_folders = {
     'Checkpoint': os.path.join("models","Stable-diffusion"),
     'LORA': os.path.join("models","Lora"),
-    'LoCon': os.path.join("models","LyCORIS"),    
-    'TextualInversion': os.path.join("embeddings"),   
-    'Hypernetwork': os.path.join("models","hypernetworks"),         
-    'AestheticGradient': os.path.join("extensions","stable-diffusion-webui-aesthetic-gradients","aesthetic_embeddings"),        
+    'LoCon': os.path.join("models","LyCORIS"),
+    'TextualInversion': os.path.join("embeddings"),
+    'Hypernetwork': os.path.join("models","hypernetworks"),
+    'AestheticGradient': os.path.join("extensions","stable-diffusion-webui-aesthetic-gradients","aesthetic_embeddings"),
     'Controlnet': os.path.join("models","ControlNet"),
     'Poses': os.path.join("models","Poses"),
-    'Wildcards': os.path.join("extensions","sd-dynamic-prompts","wildcards"),    
-    'Other': os.path.join("models","Other"),    
-    
-    'VAE': os.path.join("models","VAE"),    
-    'ANLORA': os.path.join("extensions","sd-webui-additional-networks","models","lora"),    
+    'Wildcards': os.path.join("extensions","sd-dynamic-prompts","wildcards"),
+    'Other': os.path.join("models","Other"),
+
+    'VAE': os.path.join("models","VAE"),
+    'ANLORA': os.path.join("extensions","sd-webui-additional-networks","models","lora"),
     'Unknown': os.path.join("models","Unkonwn"),
 }
 
@@ -74,7 +74,7 @@ model_folders = {
 # UI model type -> civitai model type
 
 # UI type 하나에 다중의 civitai type을 대입할때 대상이 되는것은  get_ui_typename 함수와 ishortcut->get_image_list 와 get_list 뿐이다.
-# 나머지는 key로만 쓰이기 때문에 value 값이 배열이라 해도문제가 안될듯한다. 
+# 나머지는 key로만 쓰이기 때문에 value 값이 배열이라 해도문제가 안될듯한다.
 # ishortcut 부분은 여기를
 # tmp_types.append(setting.ui_typenames[sc_type])
 # ->
@@ -102,7 +102,7 @@ ui_typenames = {
     "Other" : 'Other',
 }
 
-#information tab 
+#information tab
 civitai_information_tab = 0
 usergal_information_tab = 1
 download_information_tab = 2
@@ -130,7 +130,7 @@ shortcut_browser_search_up = False
 
 # 갤러리 ui설정
 # model browser 설정
-shortcut_column = 5  
+shortcut_column = 5
 shortcut_rows_per_page = 4
 gallery_column = 7
 
@@ -139,13 +139,13 @@ usergallery_images_column = 6
 usergallery_images_rows_per_page = 2
 
 # prompt recipe 설정
-prompt_shortcut_column = 5  
+prompt_shortcut_column = 5
 prompt_shortcut_rows_per_page = 4
 prompt_reference_shortcut_column = 8
 prompt_reference_shortcut_rows_per_page = 4
 
 # classification 설정
-classification_shortcut_column = 5  
+classification_shortcut_column = 5
 classification_shortcut_rows_per_page = 4
 
 classification_gallery_column = 8
@@ -178,7 +178,7 @@ shortcut_gallery_folder =  "sc_gallery"
 no_card_preview_image = os.path.join(extension_base,"img","card-no-preview.png")
 nsfw_disable_image = os.path.join(extension_base,"img","nsfw-no-preview.png")
 
-NSFW_filtering_enable = True 
+NSFW_filtering_enable = True
 # NSFW_level = { "None":True, "Soft":False, "Mature":False, "X":False } # None, Soft, Mature, X
 NSFW_levels = ("None","Soft","Mature","X") # None, Soft, Mature, X
 NSFW_level_user = "None"
@@ -187,36 +187,36 @@ shortcut_env = dict()
 
 def set_NSFW(enable, level="None"):
     # global NSFW_level
-    global NSFW_filtering_enable    
+    global NSFW_filtering_enable
     global NSFW_level_user
-    
+
     NSFW_filtering_enable = enable
     NSFW_level_user = level
 
 def save_NSFW():
     global NSFW_filtering_enable
     global NSFW_level_user
-    
+
     environment = load()
     if not environment:
-         environment = dict()  
-             
-    nsfw_filter = dict()    
+         environment = dict()
+
+    nsfw_filter = dict()
     nsfw_filter['nsfw_filter_enable'] = NSFW_filtering_enable
     nsfw_filter['nsfw_level'] = NSFW_level_user
-    environment['NSFW_filter'] = nsfw_filter   
-    
+    environment['NSFW_filter'] = nsfw_filter
+
     save(environment)
-         
+
 def init():
     global extension_base
-    
+
     global shortcut
     global shortcut_setting
     global shortcut_classification
     global shortcut_civitai_internet_shortcut_url
     global shortcut_recipe
-    
+
     global shortcut_thumbnail_folder
     global shortcut_recipe_folder
     global shortcut_info_folder
@@ -227,36 +227,36 @@ def init():
     shortcut_classification = os.path.join(extension_base,shortcut_classification)
     shortcut_recipe = os.path.join(extension_base,shortcut_recipe)
     shortcut_civitai_internet_shortcut_url = os.path.join(extension_base,shortcut_civitai_internet_shortcut_url)
-    
+
     shortcut_thumbnail_folder = os.path.join(extension_base,shortcut_thumbnail_folder)
     shortcut_recipe_folder = os.path.join(extension_base,shortcut_recipe_folder)
     shortcut_info_folder = os.path.join(extension_base,shortcut_info_folder)
     shortcut_gallery_folder = os.path.join(extension_base,shortcut_gallery_folder)
-    
-    load_data()        
+
+    load_data()
 
 def load_data():
     global model_folders
-   
+
     global shortcut_column
     global shortcut_rows_per_page
     global gallery_column
     global classification_shortcut_column
-    global classification_shortcut_rows_per_page    
+    global classification_shortcut_rows_per_page
     global classification_gallery_column
     global classification_gallery_rows_per_page
     global usergallery_images_column
     global usergallery_images_rows_per_page
-    
+
     global prompt_shortcut_column
     global prompt_shortcut_rows_per_page
     global prompt_reference_shortcut_column
     global prompt_reference_shortcut_rows_per_page
-            
+
     global shortcut_max_download_image_per_version
     global gallery_thumbnail_image_style
     global shortcut_browser_search_up
-    
+
     global download_images_folder
     global shortcut_browser_screen_split_ratio
     global information_gallery_height
@@ -273,32 +273,32 @@ def load_data():
         model_folders['Checkpoint'] = shared.cmd_opts.ckpt_dir
 
     if shared.cmd_opts.lora_dir:
-        model_folders['LORA'] = shared.cmd_opts.lora_dir     
-                
+        model_folders['LORA'] = shared.cmd_opts.lora_dir
+
     environment = load()
     if environment:
         if "NSFW_filter" in  environment.keys():
             nsfw_filter = environment['NSFW_filter']
             filtering_enable = True
             if 'nsfw_filter_enable' in nsfw_filter.keys():
-                filtering_enable = bool(nsfw_filter['nsfw_filter_enable'])       
-                
+                filtering_enable = bool(nsfw_filter['nsfw_filter_enable'])
+
             if 'nsfw_level' in  nsfw_filter.keys():
                 set_NSFW(filtering_enable, nsfw_filter['nsfw_level'])
-            
+
         if "application_allow" in environment.keys():
             application_allow = environment['application_allow']
 
             if "shortcut_update_when_start" in application_allow.keys():
-                shortcut_update_when_start = bool(application_allow['shortcut_update_when_start'])       
+                shortcut_update_when_start = bool(application_allow['shortcut_update_when_start'])
             if "shortcut_max_download_image_per_version" in application_allow.keys():
                 shortcut_max_download_image_per_version = int(application_allow['shortcut_max_download_image_per_version'])
-                    
+
         if "screen_style" in environment.keys():
             screen_style = environment['screen_style']
 
             if "shortcut_browser_screen_split_ratio" in screen_style.keys():
-                shortcut_browser_screen_split_ratio = int(screen_style['shortcut_browser_screen_split_ratio'])       
+                shortcut_browser_screen_split_ratio = int(screen_style['shortcut_browser_screen_split_ratio'])
             if "information_gallery_height" in screen_style.keys():
                 if screen_style['information_gallery_height'].strip():
                     information_gallery_height = screen_style['information_gallery_height']
@@ -306,7 +306,7 @@ def load_data():
                 gallery_thumbnail_image_style = screen_style['gallery_thumbnail_image_style']
             if "shortcut_browser_search_up" in screen_style.keys():
                 shortcut_browser_search_up = bool(screen_style['shortcut_browser_search_up'])
-                        
+
         if "image_style" in environment.keys():
             image_style = environment['image_style']
 
@@ -315,17 +315,17 @@ def load_data():
             if "shortcut_rows_per_page" in image_style.keys():
                 shortcut_rows_per_page = int(image_style['shortcut_rows_per_page'])
 
-            if "gallery_column" in image_style.keys():            
+            if "gallery_column" in image_style.keys():
                 gallery_column = int(image_style['gallery_column'])
 
             if "classification_shortcut_column" in image_style.keys():
                 classification_shortcut_column = int(image_style['classification_shortcut_column'])
             if "classification_shortcut_rows_per_page" in image_style.keys():
-                classification_shortcut_rows_per_page = int(image_style['classification_shortcut_rows_per_page'])                
+                classification_shortcut_rows_per_page = int(image_style['classification_shortcut_rows_per_page'])
             if "classification_gallery_column" in image_style.keys():
                 classification_gallery_column = int(image_style['classification_gallery_column'])
             if "classification_gallery_rows_per_page" in image_style.keys():
-                classification_gallery_rows_per_page = int(image_style['classification_gallery_rows_per_page'])                
+                classification_gallery_rows_per_page = int(image_style['classification_gallery_rows_per_page'])
 
             if "usergallery_images_column" in image_style.keys():
                 usergallery_images_column = int(image_style['usergallery_images_column'])
@@ -340,17 +340,17 @@ def load_data():
                 prompt_reference_shortcut_column = int(image_style['prompt_reference_shortcut_column'])
             if "prompt_reference_shortcut_rows_per_page" in image_style.keys():
                 prompt_reference_shortcut_rows_per_page = int(image_style['prompt_reference_shortcut_rows_per_page'])
-                                            
+
         if "model_folders" in environment.keys():
-                
+
             user_folders = environment['model_folders']
-            
+
             if 'LoCon' in user_folders.keys():
                 model_folders['LoCon'] = user_folders['LoCon']
-            
+
             if 'Wildcards' in user_folders.keys():
                 model_folders['Wildcards'] = user_folders['Wildcards']
-                
+
             if 'Controlnet' in user_folders.keys():
                 model_folders['Controlnet'] = user_folders['Controlnet']
 
@@ -364,26 +364,26 @@ def load_data():
                 model_folders['Other'] = user_folders['Other']
 
         if "download_folders" in environment.keys():
-                
+
             download_folders = environment['download_folders']
             if 'download_images' in download_folders.keys():
-                download_images_folder = download_folders['download_images']            
+                download_images_folder = download_folders['download_images']
 
         if "temporary" in environment.keys():
             temporary = environment['temporary']
-    
+
 def generate_type_basefolder(content_type):
-    
+
     if content_type in model_folders.keys():
-        model_folder = model_folders[content_type]        
+        model_folder = model_folders[content_type]
     elif content_type:
         model_folder = os.path.join(model_folders['Unknown'], util.replace_dirname(content_type))
     else:
-        model_folder = os.path.join(model_folders['Unknown'])                    
-                
+        model_folder = os.path.join(model_folders['Unknown'])
+
     return model_folder
-                
-def generate_version_foldername(model_name,ver_name,ver_id):      
+
+def generate_version_foldername(model_name,ver_name,ver_id):
     return f"{model_name}-{ver_name}"
 
 def get_model_folders():
@@ -405,7 +405,7 @@ def get_imagefn_and_shortcutid_from_recipe_image(recipe_image):
 def set_imagefn_and_shortcutid_for_recipe_image( shortcutid,image_fn):
     if image_fn and shortcutid:
         return f"{shortcutid}:{image_fn}"
-        
+
 def get_modelid_from_shortcutname(sc_name):
     if sc_name:
         return sc_name[sc_name.rfind(':') + 1:]
@@ -413,22 +413,22 @@ def get_modelid_from_shortcutname(sc_name):
 def set_shortcutname(modelname,modelid):
     if modelname and modelid:
         return f"{modelname}:{modelid}"
-    
+
 def get_image_url_to_shortcut_file(modelid, versionid, image_url):
     if image_url:
         version_image_prefix = f"{versionid}-"
-        model_path = os.path.join(shortcut_info_folder, str(modelid))      
+        model_path = os.path.join(shortcut_info_folder, str(modelid))
         image_id, ext = os.path.splitext(os.path.basename(image_url))
         description_img = os.path.join(model_path, f"{version_image_prefix}{image_id}{preview_image_ext}")
         return description_img
-    return None  
+    return None
 
 def get_image_url_to_gallery_file(image_url):
     if image_url:
         image_id, ext = os.path.splitext(os.path.basename(image_url))
         description_img = os.path.join(shortcut_gallery_folder, f"{image_id}{preview_image_ext}")
         return description_img
-    return None  
+    return None
 
 def save(env):
     try:
@@ -436,14 +436,14 @@ def save(env):
             json.dump(env, f, indent=4)
     except Exception as e:
         return False
-    
+
     return True
 
 def load():
-    if not os.path.isfile(shortcut_setting):        
+    if not os.path.isfile(shortcut_setting):
         save({})
         return
-    
+
     json_data = None
     try:
         with open(shortcut_setting, 'r') as f:
